7815329856aa0d61829f54d0e1d74c7a27973f53
Shubham Rathore <128213777+shubhamrathore010@users.noreply.github.com>
2024-06-14 11:43:43 +0000

    data

---
+++ basics-js/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 122334
+let accountEmail = "shubam@google.com"
+let accountPassword = "12345"
+accountCity = "Jaipur"
+let cityState;
+
+// accountId = 2
+
+accountEmail = "ges@ds.com"
+accountPassword = "23e3443"
+accountCity = "dscdcd"
+
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, cityState])
---
+++ basics-js/dataType.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+
+let name = "Shubham"
+let age = 21
+
+// data primitive Type
+
+// String = "Shubham"
+// Number = 23332
+// Boolean =  true/false
+// undefined => String = name;
+// null => standalone value 
+// Symbol => unique
+
+// object
+// null is object
+
+
+console.log(typeof "Shubham");
+console.log(typeof age);
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
+
