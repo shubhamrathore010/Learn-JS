8b7ae6c3c2732de6432ca9f9a96c9d461e345cf1
Shubham Rathore <128213777+shubhamrathore010@users.noreply.github.com>
2024-06-14 19:43:57 +0000

    #diff-dataTypes

--- basics-js/dataType.js
+++ basics-js/dataType.js
@@ -11,14 +11,52 @@ let age = 21
 // undefined => String = name;
 // null => standalone value 
 // Symbol => unique
+// bigInt => Long number
 
-// object
+
+
+// Reference {Non Primitive}
+
+// Array
+// Object
+// Functoin
 // null is object
 
 
-console.log(typeof "Shubham");
-console.log(typeof age);
-console.log(typeof null) // object
-console.log(typeof undefined) // undefined
+
+
+// console.log(typeof "Shubham");
+// console.log(typeof age);
+// console.log(typeof null) // object
+// console.log(typeof undefined) // undefined
+
+// Symbol my variable uniq from other
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const binNumber = 123232343n  //  BingInt Number representasion
+
+const heros = ["Shaktiman", "naagraj", "doga"];  // Array
+
+let myObj = {
+    name: "Shubham",
+    age: 22,
+}
+const myFunction = function() {
+    console.log("Hello wold");
+}
+
+
+
+console.log(typeof anotherId);
+//  console.log(typeof myFunction);  // => function
+// console.log(typeof myObj);
+// console.log(typeof id);  => Symbol
+// console.log(typeof heros); // => Object
 
 
