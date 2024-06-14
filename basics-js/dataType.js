3108ba4cd513ac2c0dc0d9c2a4ae79bb10fba8bd
Shubham Rathore <128213777+shubhamrathore010@users.noreply.github.com>
2024-06-14 21:35:02 +0000

    String-fun

--- basics-js/dataType.js
+++ basics-js/dataType.js
@@ -1,7 +1,7 @@
 "use strict" // treat all js code as newer version
 
-let name = "Shubham"
-let age = 21
+// let name = "Shubham"
+// let age = 21
 
 // data primitive Type
 
@@ -83,5 +83,48 @@ let userTwo = userONe
 userTwo.email = "userTwo@google.com"
 
 // console.log(userONe);
-console.log(userONe.email);
-console.log(userTwo.email);
+// console.log(userONe.email);
+// console.log(userTwo.email);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const name = "Shubham"
+const repoCount = 50
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Shubham-r-rathore')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 7)
+
+// console.log(newString);
+
+
+const anotherString = gameName.slice(3, 5)
+// console.log(anotherString);
+
+
+const newStringOne = "    Shubhan    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://shubham.com/shubham%20rathore"
+
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('sunder'));
+
+// console.log(gameName.split('-'));
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
