132087f023e9b362ed282d6980ed1d9ce60036b3
Shubham Rathore <128213777+shubhamrathore010@users.noreply.github.com>
2024-06-14 15:06:28 +0000

    #typechangr

--- basics-js/typeConversion.js
+++ basics-js/typeConversion.js
@@ -29,6 +29,21 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33;
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof  stringNumber);
 
+// let value = 3
+//  let negValue = -value
+//  console.log(negValue);
+
+//  let str1 = "hello"
+//  let str2 = "Shubham"
+
+//  let str3 = str1+str2;
+//  console.log(str3);
+
+
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "2");
+
+console.log( ( 3 + 4) * 5 % 3);
