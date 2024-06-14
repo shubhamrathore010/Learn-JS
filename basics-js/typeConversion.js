42db486f34e5e711d50d6637131fce8be4b1188e
Shubham Rathore <128213777+shubhamrathore010@users.noreply.github.com>
2024-06-14 12:33:26 +0000

    Conversion of dataType

---
+++ basics-js/typeConversion.js
@@ -0,0 +1,34 @@
+let score = "23423";
+// let valueInNumber = Number(score)
+
+
+let num = "223";
+let valueInNumber = Number(num)
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "23" => 23
+// "23423aed" => NaN
+// true => 1; false => 0
+
+
+let  isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// isLoggedIn = 1 => True
+// isLoggedIn = 0 => false
+// isLoggedIn = "" => false
+// isLoggedIn ="sonthig" => true
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
