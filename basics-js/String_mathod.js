267511dc8a46a29142bb8c60f25bbe843abeb8db
Shubham Rathore <128213777+shubhamrathore010@users.noreply.github.com>
2024-06-16 19:31:21 +0000

    #String_method

---
+++ basics-js/String_mathod.js
@@ -0,0 +1,38 @@
+// let name = "qwertyuioplkjhgfdsaxcvbnm";
+// let length = name.length;
+
+// console.log(length);
+
+let text = "Hello WorlLd and WorShubham";
+// console.log(text.charAt(0));
+// console.log(text.charCodeAt(0));
+// console.log(text.at(0));
+// console.log(text[0]);
+
+// console.log(text.slice(0, 4));
+
+// console.log(text, text.substring(0, 4),text);
+
+// console.log(text.substr(-4,2));  // => substr start if - behind counting then print end 
+// console.log(text.substr(-4));
+
+// let text = "5"
+// console.log(text.padEnd(3, "x"));
+
+// console.log(text.replace("World","Shubham"));
+// console.log(text.split(""),text);
+
+//  let newtext = text.indexOf("World");
+
+//  console.log(text.indexOf("World"))
+// console.log(text.lastIndexOf("World"))
+
+//  console.log(text.search("rld"));
+
+// console.log(text.match(/ll/gi));  // =>search globely case insensitive
+
+// console.log(text.includes("and"));   // => true ro false
+
+console.log(text.startsWith("and"));
+
+
