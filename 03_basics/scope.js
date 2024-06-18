4c11c080cd9f1d3b8c00b27466aa18fcd9443f52
Shubham Rathore <128213777+shubhamrathore010@users.noreply.github.com>
2024-06-18 00:56:41 +0000

    #scope

---
+++ 03_basics/scope.js
@@ -0,0 +1,40 @@
+function one() {
+ const username = "Shubham"
+
+    function two() {
+        let webname = "youtube"
+      console.log(username);
+    }
+    two()
+    // console.log(webname);
+}
+
+// one()
+
+if (true) {
+    const username = "Shubham"
+
+    if(username === "Shubham") {
+        const lastname = " Rathore"
+        // console.log(username + lastname);
+    }
+    // console.log(lastname);
+}
+// console.log(username);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++
+
+
+
+console.log(addone(2))
+
+function addone(num) {
+    return num +1
+}
+
+fun(2)
+const fun = function(num) {
+    return num +2
+}
+
