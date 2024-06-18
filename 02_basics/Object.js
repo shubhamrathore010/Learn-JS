4fb401b66b5385ce91edc91a7bc50b4b2a80e079
Shubham Rathore <128213777+shubhamrathore010@users.noreply.github.com>
2024-06-18 14:05:23 +0000

    #Object_method

--- 02_basics/Object.js
+++ 02_basics/Object.js
@@ -1,17 +1,17 @@
 // object literal
 
-// const mySym =  Symbol("Key1")  // define symbol  
+const mySym =  Symbol("Key1")  // define symbol  
 
-// const user = {
+const user = {
 //     Name : "shubham",
 //    "full name": "Shubham Rathore",
-//    [mySym]: "mykey1",   // Symbol act as a key
-//     age: 18,
-//     location: "indore",
-//     email: "Suhbham@gmail.com",
-//     isLoggedIn: false,
-//     lastLoginDays: ["Monday", "Saturday"]
-// }
+   [mySym]: "mykey1",   // Symbol act as a key
+    // age: 18,
+    // location: "indore",
+    // email: "Suhbham@gmail.com",
+    // isLoggedIn: false,
+    // lastLoginDays: ["Monday", "Saturday"]
+}
 // user.email = "shubham@chatgpt.com"
 // user.Name = "Shubham"
 // // Object.freeze(user)
@@ -57,15 +57,15 @@ tinderUser.isLoggedIn = false
 
 // nasted object
 
-const regulerUser = {
-    email: "name@gmail.com",
-    fullname: {
-        userfullname: {
-            firstname: "Shubham",
-            lastname: "Rathore"
-        }
-    }
-}
+// const regulerUser = {
+//     email: "name@gmail.com",
+//     fullname: {
+//         userfullname: {
+//             firstname: "Shubham",
+//             lastname: "Rathore"
+//         }
+//     }
+// }
 // console.log(regulerUser.fullname);
 // console.log(regulerUser.fullname.userfullname.firstname);
 
@@ -73,10 +73,10 @@ const obj1 = {1:"a",2: "b"}
 const obj2 = {3:"a",4: "b"}
 
 // const obj3 = {obj1, obj2}
-// const obj3 = Object.assign({}, obj1, obj2)  // traget/assing
+const obj3 = Object.assign({}, obj1, obj2)  // traget/assing
 // const obj3 = {...obj1, ...obj2}
 
-// console.log(obj3);
+// console.log(obj3, obj1);
 
 
 // console.log(tinderUser);
@@ -86,13 +86,54 @@ const obj2 = {3:"a",4: "b"}
 // console.log(Object.entries(tinderUser));
 // console.log(Object.hasOwnProperty("name"));
 
-const course = {
-    courename: "js in hindi",
-    price: "999",
-    couresir: "hitesh"
-}
-const {couresir: sir} = course   // => desstructor
+// const course = {
+//     courename: "js in hindi",
+//     price: "999",
+//     couresir: "hitesh"
+// }
+// const {couresir: sir} = course   // => desstructor
 
-// console.log(sir);
+// // console.log(sir);
+// let data = course.constructor;
+// console.log(data);
 
+
+// const person1 = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+//   };
+  
+  // Create Source Object
+//   const person2 = {FirstName: "Anne",LastName: "Smith"};
+//   const person2 = {firstName: "Anne",lastName: "Smith"};
+  // Assign Source to Target
+//   console.log(Object.assign(person1, person2));
+
+//  const Keys = Symbol("Chabi")
+
+//  const Data = {
+//     [Keys]: "Keys"
+//  }
+//  console.log(Data);
+
+
+const person1 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+  
+
+  const man = Object.create(person1);
+  man.firstName = "Peter";
+
+// console.log(man);
+ Object.defineProperties(person1, {
+    language: {value: "en"},
+    year: {value: "Hello"}
+ });
+ console.log(person1);
 
