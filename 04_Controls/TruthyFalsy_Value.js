03787c0a9d72c4d9d9d1673704b2c62023877598
Shubham Rathore <128213777+shubhamrathore010@users.noreply.github.com>
2024-06-18 20:30:16 +0000

    #TruFAlse

--- 04_Controls/TruthyFalsy_Value.js
+++ 04_Controls/TruthyFalsy_Value.js
@@ -1,5 +1,55 @@
-const EmailLoggedIn = "Shubham@gmail.com"
+const useremail = []
 
-if(EmailLoggedIn) {
-    console.log("user loggedin");
-}
+// if(useremail) {
+//     console.log("user loggedin");
+// } else {
+//     console.log("Dont login");
+// }
+
+//falsy value
+
+//  false, 0 -0, "", null ,undefined, NaN, BigInt 0n,
+
+// Truthy Value
+
+// '0', " ", "false", [],{}, function(){}
+
+// if(useremail.length === 0) {
+//     console.log("Array i empty");
+// }
+
+// const emptobj = {}
+
+//  if(Object.keys(emptobj).length === 0) {
+//     console.log("Oblect is empty");
+//  }
+
+
+// Nullish Coalescing (??): null undefined
+
+let val1;
+
+val1 = 5 ?? 10;
+// console.log(val1);
+
+val1 = null ?? 10;
+// console.log(val1);
+
+val1 = undefined ?? 12;
+// console.log(val1);
+
+val1 = null ?? 12 ?? 42;
+console.log(val1);
+
+ 
+// Ternery operator
+// 
+// condition ?  true: false
+
+
+const iceT = 100
+
+// iceT <= 80 ? console.log("less then 80") : console.log("more Than 80");
+
+  
+
